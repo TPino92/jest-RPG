27a0700ebf961cf68e85bd9881fefbbe40a66f64
Trevor Pino <trevorpino@yahoo.com>
2020-09-28 16:30:02 -0600

    created basic player object and functions

---
+++ __tests__/Player.test.js
@@ -0,0 +1,17 @@
+const { TestScheduler } = require('jest');
+const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+test('creates a player object', () => {
+const player = new Player('Trevor');
+
+expect(player.name).toBe('Trevor');
+expect(player.health).toEqual(expect.any(Number));
+expect(player.strength).toEqual(expect.any(Number));
+expect(player.agility).toEqual(expect.any(Number));
+expect(player.inventory).toEqual(
+    expect.arrayContaining([expect.any(Object)])
+);
+});
